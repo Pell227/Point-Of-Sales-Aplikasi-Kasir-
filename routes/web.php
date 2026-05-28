e8fceed9b0be4b950045ed992ebcc4eb313160aa
Azzarqyfzrn <azzarqyf@gmail.com>
2026-05-28 13:37:49 +0700

    payment method 2

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentMethodsController;
 
+Route::get('/', function () {
+    return redirect('/paymentMethods');
+});
+
 Route::resource('paymentMethods', PaymentMethodsController::class);
