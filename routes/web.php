28aa36f8a4dc86e0ee50dfd14474b93182cbe5b2
windriew <windriew@gmail.com>
2026-05-28 18:53:21 +0700

    buat api product

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/products', function () {
+    $products = Product::all();
+    return view('products', compact('products'));
+});
+
+Route::post('/products', function (Request $request) {
+
+    Product::create([
+        'name' => $request->name,
+        'stock' => $request->stock,
+        'price' => $request->price,
+    ]);
+
+    return redirect('/products');
 });
