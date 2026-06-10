1ca420a20500cacc227a223d2f65e06661ddc7de
Pelll27 <felisiakhe@gmail.com>
2026-06-10 16:38:02 +0700

    Added Main menu and fix on transaction database

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\LoginWebController;
 use App\Http\Controllers\UserWebController;
 
+Route::get('/cek-menu', function () {
+    return view('main');
+});
+
 Route::post('/products', function (Request $request) {
 
     Product::create([
@@ -26,13 +30,9 @@ Route::get('/products', function () {
 });
 
 
-Route::get('/', [StaffController::class, 'index']);
+Route::get('/staff', [StaffController::class, 'index']);
 
-Route::get('/', function () {
-    return redirect('/login');
-});
-
-Route::get('/login', [LoginWebController::class, 'showLogin'])->name('login');
+Route::get('/', [LoginWebController::class, 'showLogin'])->name('login');
 Route::post('/login', [LoginWebController::class, 'login'])->name('login.post');
 Route::post('/logout-web', [LoginWebController::class, 'logoutWeb'])->name('logout.web');
 
