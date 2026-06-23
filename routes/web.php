b39efc0b0052a0ed09de13331506e9605f19a0dc
Pelll27 <felisiakhe@gmail.com>
2026-06-23 14:36:37 +0700

    Link some methods

--- routes/web.php
+++ routes/web.php
@@ -56,12 +56,10 @@ Route::middleware('auth')->group(function () {
     Route::post('/cart/add/{id}', [CartController::class, 'add'])
         ->name('cart.add');
 
-    Route::post('/cart/update/{id}', [CartController::class, 'update'])
-        ->name('cart.update');
+    Route::post('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
 
-    Route::post('/cart/remove/{id}', [CartController::class, 'remove'])
-        ->name('cart.remove');
+    Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
 
-    Route::post('/checkout', [CartController::class, 'checkout'])
-        ->name('checkout');
+    Route::get('/checkout', [CartController::class, 'checkout'])->name('checkout.form');
+    Route::post('/checkout/process', [CartController::class, 'processCheckout'])->name('checkout.process');
 });
